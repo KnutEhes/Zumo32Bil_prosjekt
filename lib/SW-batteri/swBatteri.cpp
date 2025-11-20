a3a40e261a18bacb2ce97dbd047b9f6b04e756e2
edvardrb <edvardrb06@gmail.com>
2025-11-20 13:24:28 +0100

    wallah

--- lib/SW-batteri/swBatteri.cpp
+++ lib/SW-batteri/swBatteri.cpp
@@ -5,6 +5,7 @@ Zumo32U4LCD lcd;
 
 int batteryHealth = 100;
 float batteryPersentVal = 100;
+float prevBatteryPersentVal = 100;
 
 float acceleration = 0;
 int speed = 0;
@@ -15,9 +16,15 @@ unsigned long lastSpeedCheck = 0;
 float fastChrargingPrice = 1.5 * sin(0.0008 * time + 5) + 8 + 0.00001 * time;
 float slowChargingPrice = 1.5 * sin(0.001 * time + 13) + 5 + 0.00001 * time;
 
+//Batteriet kan ikke lade opp høyere enn batteryHealth
 void cycles()
 {
+    if((batteryPersentVal < 20 && prevBatteryPersentVal > 20) || (prevBatteryPersentVal > 80 && prevBatteryPersentVal < 80)){
+        batteryHealth -= 1;
+    }
+
 }
+
 
 void charging()
 {
@@ -31,14 +38,17 @@ void batteryPercent()
     {
         Motion M = calculateMotion();
         batteryPersentVal -= kSpeed* abs(M.speed)+ kAcc* abs(M.acceleration);
-
+        
+        cycles();
+        
         lcd.clear();
         lcd.gotoXY(0,0);
         lcd.println(batteryPersentVal);
         lcd.println(M.acceleration);
         lcd.println(M.speed);
         lcd.println(M.distance);
-
+        
+        prevBatteryPersentVal = batteryPersentVal;
         lastSpeedCheck = time;
     }
 }

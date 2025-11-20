ec5a15185b62ed4e198f8ebae0e971b98dbfa808
edvardrb <edvardrb06@gmail.com>
2025-11-20 18:59:35 +0100

    #

--- lib/PID-Controller/PID.cpp
+++ lib/PID-Controller/PID.cpp
@@ -1,7 +1,7 @@
 //
 // Created by knuth on 14.11.2025.
 //
-
+/* funker ikke?
 #include <cstdint>
 int kp = 6;
 int ki = 10;
@@ -11,3 +11,4 @@ int16_t lastError = 0;
 int16_t speedDifference(int16_t error, int16_t lastError) {
     error / kp + ki * (error - lastError);
 }
+*/

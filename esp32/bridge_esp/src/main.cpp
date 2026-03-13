0487f83df41d9651377d29498dc29bc2588c2df0
edvardrb <edvardrb06@gmail.com>
2026-03-13 14:32:18 +0100

    espnow kommunikasjon

--- esp32/bridge_esp/src/main.cpp
+++ esp32/bridge_esp/src/main.cpp
@@ -1,18 +1,134 @@
-#include <Arduino.h>
+#include <esp_now.h>
+#include <WiFi.h>
+#include <WebSocketsClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+WebSocketsClient webSocket;
+
+// 1. MAC-ADRESSE TIL MOTTAKER
+uint8_t broadcastAddress[] = {0x84, 0x1F, 0xE8, 0x3A, 0x44, 0x28};
+
+// Strukturer
+typedef struct send_message {
+    bool trafficLightGreen;
+    char nextTurn;
+    char posName;
+} send_message;
+
+typedef struct receive_message {
+    float balance;
+    float speed;
+    float batteryLevel;
+    uint16_t iceCreams;
+} receive_message;
+
+send_message infoToSend;
+receive_message incomingInfo;
+esp_now_peer_info_t peerInfo;
+
+// WebSocket Event Handler
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED:
+      Serial.println("[WS] Disconnected!");
+      break;
+    case WStype_CONNECTED:
+      Serial.println("[WS] Connected to RPi!");
+      break;
+    case WStype_TEXT:
+      Serial.printf("[WS] Melding fra RPi: %s\n", payload);
+      break;
+  }
+}
+
+// Callback når data er sendt
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+// Callback når data er mottatt (Fikset for eldre/standard versjon)
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&incomingInfo, incomingData, sizeof(incomingInfo));
+  
+  // Vi lager en tekststreng av dataene for å sende sikkert til RPi
+  String wsPayload = String(incomingInfo.balance) + "," + 
+                     String(incomingInfo.speed) + "," + 
+                     String(incomingInfo.batteryLevel) + "," + 
+                     String(incomingInfo.iceCreams);
+  
+  webSocket.sendTXT(wsPayload); // Sender som tekst til Raspberry Pi
+  Serial.println("Data videresendt til RPi: " + wsPayload);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  // WiFi må settes opp før ESP-NOW
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("Ditt_SSID", "Ditt_Passord");
+  
+  Serial.print("Kobler til WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi tilkoblet!");
+
+  // Initialiser ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Registrer callbacks - fjernet unødvendig typecasting som ofte feiler
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_register_recv_cb(OnDataRecv);
+  
+  // Legg til peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0; // Bruker samme kanal som WiFi
+  peerInfo.encrypt = false;
+  
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  
+  // WebSocket setup (husk å endre IP!)
+  webSocket.begin("192.168.x.x", 81, "/"); 
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setReconnectInterval(5000); // Prøv på nytt hvert 5. sek hvis den faller ut
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  webSocket.loop();
+
+  // Oppdater data som skal sendes
+  infoToSend.trafficLightGreen = false;
+  infoToSend.nextTurn = 'L';
+  infoToSend.posName = 'H';
+
+  // Send hvert 5. sekund for å ikke floode nettverket mens du tester
+  static unsigned long lastSend = 0;
+  if (millis() - lastSend > 5000) {
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &infoToSend, sizeof(infoToSend));
+    lastSend = millis();
+    
+    if (result == ESP_OK) {
+      Serial.println("ESP-NOW sendt suksessfullt");
+    } else {
+      Serial.println("ESP-NOW send feilet");
+    }
+    
+    printIncomingData();
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void printIncomingData(){
+  Serial.println("--- SISTE DATA MOTTATT ---");
+  Serial.print("Balance: "); Serial.println(incomingInfo.balance);
+  Serial.print("Speed: "); Serial.println(incomingInfo.speed);
+  Serial.print("Battery: "); Serial.print(incomingInfo.batteryLevel); Serial.println("%");
+  Serial.print("Ice Creams: "); Serial.println(incomingInfo.iceCreams);
+  Serial.println("--------------------------");
 }
--- esp32/zumo_esp/src/main.cpp
+++ esp32/zumo_esp/src/main.cpp
@@ -1,18 +1,158 @@
-#include <Arduino.h>
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/esp-now-two-way-communication-esp32/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+#include <esp_now.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
+void updateDisplay();
+void getReadings();
+
+// REPLACE WITH THE MAC Address of your receiver 
+uint8_t broadcastAddress[] = {0x64, 0xB7, 0x08, 0x29, 0x1A, 0x2C};
+
+// Define variables to store BME280 readings to be sent
+float balance;
+float speed;
+float batteryLevel;
+uint16_t iceCreams;
+
+// Define variables to store incoming readings
+bool trafficLightGreen;
+char nextTurn; //Sier hvilken vei du skal svinge i neste sving
+char posName; //Sier om man kjører, er på lader eller selger is osv.
+
+// Variable to store if sending data was successful
+String success;
+
+//Structure example to send data
+//Must match the receiver structure
+typedef struct send_message {
+    float balance;
+    float speed;
+    float batteryLevel;
+    uint16_t iceCreams;
+} send_message;
+
+typedef struct recive_message {
+    bool trafficLightGreen;
+    char nextTurn;
+    char posName;
+} recive_message;
+
+// Create a struct_message called BME280Readings to hold sensor readings
+send_message zumoInfo;
+
+// Create a struct_message to hold incoming sensor readings
+recive_message incomingInfo;
+
+esp_now_peer_info_t peerInfo;
+
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status ==0){
+    success = "Delivery Success :)";
+  }
+  else{
+    success = "Delivery Fail :(";
+  }
+}
+
+// Callback when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&incomingInfo, incomingData, sizeof(incomingInfo));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  char incomingDir = incomingInfo.nextTurn;
+  char incomingPosInfo = incomingInfo.posName;
+  bool incomingTLS = incomingInfo.trafficLightGreen;
+}
+ 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+  // Init Serial Monitor
+  Serial.begin(115200);
 
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Hvis ruteren din er på kanal 6, skriv 6 her. 
+  // Du kan finne kanalen ved å skrive Serial.println(WiFi.channel()) på Gatewayen.
+  int channel = 6; 
+  esp_wifi_set_promiscuous(true);
+  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+  esp_wifi_set_promiscuous(false);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(esp_now_send_cb_t(OnDataSent));
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+}
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
+  getReadings();
+ 
+  // Set values to send
+  zumoInfo.balance = balance;
+  zumoInfo.batteryLevel = batteryLevel;
+  zumoInfo.iceCreams = iceCreams;
+  zumoInfo.speed = speed;
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &zumoInfo, sizeof(zumoInfo));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  updateDisplay();
+  delay(1000);
+}
+void getReadings(){//Fa info fra ledning kommunikasjon er
+  balance = 1000.0;
+  batteryLevel = 67.0;
+  iceCreams = 37;
+  speed = 112.3;
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void updateDisplay(){
+  
+  // Display Readings in Serial Monitor
+  Serial.println("INCOMING READINGS");
+  Serial.print("Neste sving: ");
+  Serial.print(incomingInfo.nextTurn);
+  Serial.println(" ");
+  Serial.print("Posisjon nå: ");
+  Serial.print(incomingInfo.posName);
+  Serial.println(" ");
+  Serial.print("Neste Trafikklys er grønt: ");
+  Serial.print(incomingInfo.trafficLightGreen);
+  Serial.println(" ");
+  Serial.println();
 }

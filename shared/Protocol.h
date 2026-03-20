f9230fda9638527c5bd57b76f539552839f5b03f
edvardrb <edvardrb06@gmail.com>
2026-03-20 14:09:47 +0100

    shared fil endret

--- shared/Protocol.h
+++ shared/Protocol.h
@@ -8,3 +8,20 @@ struct Isbil {  //totalt 16 bytes av en eller anna grunn
     Isbil(float b = 0, float s = 0, float bL = 0, uint16_t i = 0) 
         : balance(b),  speed(s), batteryLevel(bL), iceCreams(i) {}
 };
+
+
+typedef struct recive_message {
+    bool trafficLightGreen;
+    char nextTurn;
+    char posName;
+
+    recive_message(bool TLG, char NT, char PN)
+        : trafficLightGreen(TLG), nextTurn(NT), posName(PN) {}
+
+} recive_message;
+
+
+
+typedef struct {
+    char json[ESPNOW_MAX_LEN];
+} EspNowMessage;
